6ee0838d8efa1be1838f79ab6b735fea672be724
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-22 08:26:49 +0700

    Create tutorial.php

---
+++ tutorial.php
@@ -0,0 +1,21 @@
+<?php
+// tutorial testing php
+$nama = "randy";
+$umur = "19";
+$hobi = "developer";
+
+$full_lengkap = "{nama} {umur} {hobi}";
+
+// run 
+echo $full_lengkap
+
+
+// Checking
+$goblok = true;
+
+if ($goblok) {
+  echo "something";
+}
+
+?>
+
